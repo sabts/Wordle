1b3f8f4a9c48d83957617dec1fc46d10ee0178e4
sabts <sabrinataraschi@gmail.com>
2025-04-16 11:42:30 +0200

    cambiando color

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
-const gameBoardElement = document.getElementById('gameBoard');
-const formElement = document.getElementById('form')
-const errorTextElement = document.getElementById('error-message')
+const gameBoardElement = document.getElementById("gameBoard");
+const formElement = document.getElementById("form");
+const errorTextElement = document.getElementById("error-message");
 const typeZoneElement = document.getElementById("typeZone");
 
 const ALL_WORDS = [
@@ -21,28 +21,30 @@ const ALL_WORDS = [
   "hongo",
   "cordyceps",
 ];
-let randomWord = ""; 
+
+let randomWord = "";
+const tries = 6;
 let actualRow = 0;
 
 //1 Seleccionar palabra aleatoria
 const randomWordToGuess = () => {
   randomWord = ALL_WORDS[Math.floor(Math.random() * ALL_WORDS.length)];
-  console.log('palabra elegida ' + randomWord);
+  console.log("palabra elegida " + randomWord);
   return randomWord;
 };
 
 //2. Crear el tablero: primero esta el padre(el game-board),luego el hijo(game-board-row) y de esos esta el hijo del hijo que es tile(va dendro de game-board-row)
 const createBoard = () => {
-  const letters = randomWord.split('');
+  const letters = randomWord.split("");
 
-  for (let i = 0; i < 6; i++) {
+  for (let i = 0; i < tries; i++) {
     const fragment = document.createDocumentFragment();
-    const rowElement = document.createElement('div');
-    rowElement.classList.add('row');
-
+    const rowElement = document.createElement("div");
+    rowElement.classList.add("row");
+    //usar J
     letters.forEach(letter => {
-      const letterElement = document.createElement('span');
-      letterElement.classList.add('tile');
+      const letterElement = document.createElement("span");
+      letterElement.classList.add("tile");
       rowElement.append(letterElement);
     });
 
@@ -53,44 +55,56 @@ const createBoard = () => {
 //3.Recoger el valor del input
 //4. Pintar lo q habéis escrito en el tablero, cada letra en el cuadradito
 const InputTypedText = () => {
-  event.preventDefault()
+  event.preventDefault();
 
-if(typeZoneElement.value.length !== randomWord.length){
-  errorTextElement.classList.remove('hide')
-  errorTextElement.textContent = `The word doesn't have ${randomWord.length} characters`
-} else {
-  errorTextElement.classList.add('hide');
-  const rows = document.querySelectorAll('.row')[actualRow]; // El padre. Me aseguro que comienza por la primera fila
-  const tiles = rows.querySelectorAll('.tile'); // selecciono los hijos que estan dentro del padre
-  for(let i=0; i < randomWord.length;i++){
-    tiles[i].textContent = typeZoneElement.value[i];
+  if (typeZoneElement.value.length !== randomWord.length) {
+    errorTextElement.classList.remove("hide");
+    errorTextElement.textContent = `The word doesn't have ${randomWord.length} characters`;
+  } else {
+    errorTextElement.classList.add("hide");
+    const rows = document.querySelectorAll(".row")[actualRow]; // El padre. Me aseguro que comienza por la primera fila
+    const tiles = rows.querySelectorAll(".tile"); // selecciono los hijos que estan dentro del padre
+    for (let i = 0; i < randomWord.length; i++) {
+      tiles[i].textContent = typeZoneElement.value[i];
+    }
+    actualRow++;
   }
-  actualRow++
-}
+  event.target.reset();
 };
-//5. Color de los cuadrados 
-//usar el root y las variables.
-//includes intentar crear un rootStyles de los colores y borrar lineas en el css
+//5. Color de los cuadrados
+//El primer blucle revisa si esta correcto
+//segundo el amarillo(la letra esta pero en el lugar incorrecto)
+// No esta (gris)
 const tilesChangeColor = () => {
-  const rows = document.querySelectorAll('.row')[actualRow]; //me coloca el color abajo
-  const tiles = rows.querySelectorAll('.tile');
+  //const tiles = gameBoardElement.children[1].children[actualRow].children; // es un array, se tiene que crear un bucle del nieto (ultimo hijo)
 
-  for(let i=0; i < randomWord.length;i++){
-    if(typeZoneElement.value[i] === randomWord[i]){
-      tiles[i].classList.add('tile-correct');
-    } else if(randomWord.includes(typeZoneElement.value[i]) === randomWord[i]){
-      tiles[i].classList.add('tile-present'); //no me lee el amarillo
-    }else{
-      tiles[i].classList.add('tile-incorrect')
+  const lettersInBoard = randomWord;
+  console.log("palabra del board: " + lettersInBoard);
+
+  for (let i = 0; i < randomWord; index++) {
+    const selectedWord =
+      gameBoardElement.children[1].children[actualRow].children;
+    console.log("esta es la palabra seleccionada: " + selectedWord);
+  }
+
+  for (let j = 0; 0 < typeZoneElement.value; j++) {
+    const typeWord = typeZoneElement.textContent[i];
+
+    if (selectedWord !== typeWord) {
+      tiles.classList.add("tile-incorrect");
     }
   }
-}
+};
+//clases:
+//  tiles[i].classList.add("tile-correct"); Verde
+//  tiles[i].classList.add("tile-present"); Amarillo
+//   tiles[i].classList.add("tile-incorrect"); Gris
 
-//6. Ganar o perder //replace errorTextElement con You WIN, You LOSE
+//6. Ganar o perder //text content errorTextElement con You WIN, You LOSE
 
 randomWordToGuess();
 createBoard();
-tilesChangeColor()  
+tilesChangeColor();
 
-formElement.addEventListener('submit',InputTypedText)
-formElement.addEventListener('submit',tilesChangeColor)
+formElement.addEventListener("submit", InputTypedText);
+formElement.addEventListener("submit", tilesChangeColor);
